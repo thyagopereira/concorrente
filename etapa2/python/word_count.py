c0df54fe76eabf118d61e233fb7ccffa81f86233
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2023-09-22 22:05:40 -0300

    Solucao em python

--- etapa2/python/word_count.py
+++ etapa2/python/word_count.py
@@ -1,34 +1,55 @@
 import os
 import sys
-
-def wc(content):
-    return len(content.split())
+import time
+from threading import Thread
+from threading import Barrier 
+from threading import Semaphore
 
 def wc_file(filename):
+    global count
     try:
+
         with open(filename, 'r', encoding='latin-1') as f:
             file_content = f.read()
-        return wc(file_content)
+        
+        mutex.acquire()
+        count += len(file_content.split())
+        mutex.release()
+
     except FileNotFoundError:
         return 0
 
 def wc_dir(dir_path):
-    count = 0
+    
+    threads = []
     for filename in os.listdir(dir_path):
         filepath = os.path.join(dir_path, filename)
+
         if os.path.isfile(filepath):
-            count += wc_file(filepath)
+            threads.append(Thread(target= wc_file, args=[filepath]))
         elif os.path.isdir(filepath):
-            count += wc_dir(filepath)  # Chamada recursiva para diretórios
-    return count
+            threads.append(Thread(target= wc_dir, args=[filepath])) 
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join() 
+    
+
+  # Chamada recursiva para diretórios
 
 def main():
+    global count
+
     if len(sys.argv) != 2:
         print("Usage: python", sys.argv[0], "root_directory_path")
         return
     root_path = os.path.abspath(sys.argv[1])
-    count = wc_dir(root_path)
-    print(count)
+    wc_dir(root_path)
 
 if __name__ == "__main__":
+    count = 0
+    mutex = Semaphore() 
     main()
+    print(count)
